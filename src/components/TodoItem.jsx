89e8a733e9e6ab83301245080af4843538379a7e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-20 18:43:52 +0300

    Add funtionality to edit list items

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+  const [editing, setEditing] = useState(false);
+
   const completedStyle = {
     fontStyle: 'italic',
     color: '#595959',
@@ -8,17 +11,46 @@ const TodoItem = ({ itemProp, handleChange, delTodo }) => {
     textDecoration: 'line-through',
   };
 
-    return (
-      <li className={styles.item}>
-        <div className={styles.content}>
-          <input type="checkbox" 
-          checked={itemProp.completed}
-          onChange={() => handleChange(itemProp.id)}
-          />
-          <span style={itemProp.completed ? completedStyle : null}>{itemProp.title}</span>
-          <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-        </div>
-      </li>);
+  const handleEditing = () => {
+    setEditing(true);
   };
-  export default TodoItem;
+
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input type="checkbox" 
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+        />
+        <span style={itemProp.completed ? completedStyle : null}>{itemProp.title}</span>
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      </div>
+      <input
+        type="text"
+        value={itemProp.title}
+        className={styles.textInput}
+        style={viewMode}
+        onChange={(e) => setUpdate(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+export default TodoItem;
   
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -52,6 +52,17 @@ const TodosLogic = () => {
       setTodos([...todos, newTodo]);
     };
 
+    const setUpdate = (updatedTitle, id) => {
+      setTodos(
+        todos.map((todo) => {
+          if (todo.id === id) {
+            todo.title = updatedTitle;
+          }
+          return todo;
+        })
+      );
+    };
+
     return (
         <div>
         < InputTodo 
@@ -61,6 +72,7 @@ const TodosLogic = () => {
         todosProps={todos} 
         handleChange={handleChange}
         delTodo={delTodo}
+        setUpdate={setUpdate}
         />
       </div>
     )

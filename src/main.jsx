504cc5971e79398012bc26bcc46d8b9f2048744e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-20 15:25:41 +0300

    Add Navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoApp from '@/components/TodoApp';
+import Navbar from "./components/Navbar";
 
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
 <React.StrictMode>
+    <Navbar />
     <TodoApp />
 </React.StrictMode>
 );

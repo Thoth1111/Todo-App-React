562fd9e9ef394d9def7fad97ac943a217de3f2d8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-20 18:03:36 +0300

    Style App using module and inline css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoApp from '@/components/TodoApp';
+import '@/styles/App.css';
 
 
 const domContainer = document.getElementById("root");
